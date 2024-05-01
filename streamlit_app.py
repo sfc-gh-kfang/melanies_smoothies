af076268b79d2296fb5c4d31b227faaafd78e52d
Kate Fang <122835750+sfc-gh-kfang@users.noreply.github.com>
2024-05-01 15:56:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         a = []
         a.append(fruityvice_response.json())
         st.write(a)
-        fv = pd.DataFrame(a, columns = ['nutritions'])
+        fv = pd.DataFrame(a, columns = ['nutritions']).json()
         st.write(fv)
         # fv_2=fv.drop(columns=['family'])
         fv_df_2 = st.dataframe(data=fv, use_container_width=True)
