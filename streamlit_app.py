f2a91ee3c731f59abbe2cfdfb8f2e70a3217911c
Kate Fang <122835750+sfc-gh-kfang@users.noreply.github.com>
2024-05-01 19:46:11 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
+import streamlit.components.v1 as components
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -52,6 +53,7 @@ if ingredients_list:
         
        # st.write(fv)
         st.write(fvv)
+        components.html(fvv.to_html(header=False))
        # st.write(pd.json_normalize(a["nutritions"]))
        # fv_nut = pd.json_normalize(fv["labels"])
        # st.write(fv_nut)
