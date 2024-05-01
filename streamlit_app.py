1e9d0ade4be50c82a1dbd3d146c99ebc9d598796
Kate Fang <122835750+sfc-gh-kfang@users.noreply.github.com>
2024-05-01 14:36:39 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nurition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        df = fruityvice_response.json()
-        fv_df = st.dataframe(df[["nutritions"]], use_container_width=True)
+        fv_df = st.dataframe(data=fruityvice_response.json(["nutritions"]), use_container_width=True)
 
     #st.write(ingredients_string)
 
